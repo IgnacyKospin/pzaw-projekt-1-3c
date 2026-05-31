21b093442e0705d896bf72fded9f17e293f7f1a9
IgnacyKospin <ignacykospin@gmail.com>
2026-05-31 11:31:58 +0200

    basic, desired functionality done, will revamp readme and add econ calculations when i return from swimming

--- projekt05/models/intercategorial.js
+++ projekt05/models/intercategorial.js
@@ -1,9 +1,16 @@
 import db from "./database.js";
 const internal_dboperations = {
-    get_all_categories: db.prepare("SELECT name, key FROM economic_categories;")
+    get_all_categories: db.prepare("SELECT name, key FROM economic_categories;"),
+    aggregate_facilities_resource_movement: db.prepare("SELECT * FROM facilities JOIN production_methods ON facilities.production_method_key = production_methods.key;")
 }
 export function export_categories(){
     return internal_dboperations.get_all_categories.all();
+}
+/**
+ * This aggregates the total production and consumption of all facilities, and updates their respective goods entry.
+ */
+function theGreatEconomicUpdater(){
+ //process for when i do this during the evening: get aggregate_facilities_resource_movement, convert them through production_methods function (i think?), multiply each by facility level, merge input/output, update goods.
 }
 export default {
     export_categories
